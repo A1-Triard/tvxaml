7a37edcb7ae0c99396a37f55d08c6e816fda0e46
Master <internalmike@gmail.com>
2025-06-07 23:00:18 +0300

    peek for avoid open/close mess

--- src/xaml.rs
+++ src/xaml.rs
@@ -1,9 +1,9 @@
 use anycase::to_snake;
-use either::{Either, Left, Right};
 use serde::{Deserializer, Deserialize, serde_if_integer128};
 use serde::de::{self, DeserializeSeed, IntoDeserializer};
 use serde::de::value::{StringDeserializer, StrDeserializer};
 use std::fmt::{self, Display, Formatter};
+use std::mem::replace;
 use std::vec::{self};
 use tvxaml_screen_base::trim_text;
 
@@ -45,35 +45,44 @@ impl de::Error for Error {
     fn custom<T: Display>(msg: T) -> Self { Error::Custom(format!("{msg}")) }
 }
 
-fn skip_whitespace<S: Iterator<Item=u8>>(
-    reader: &mut no_std_xml::EventReader<S>
-) -> Result<no_std_xml::reader::XmlEvent, Error> {
-    loop {
-        let e = reader.next()?;
-        if let no_std_xml::reader::XmlEvent::Whitespace(_) = &e {
-        } else {
-            break Ok(e);
+struct Reader<S: Iterator<Item=u8>> {
+    inner: no_std_xml::EventReader<S>,
+    next: no_std_xml::reader::XmlEvent,
+}
+
+impl<S: Iterator<Item=u8>> Reader<S> {
+    fn new(mut inner: no_std_xml::EventReader<S>) -> Result<Self, Error> {
+        let next = inner.next()?;
+        Ok(Reader {
+            inner,
+            next
+        })
+    }
+
+    fn peek(&self) -> &no_std_xml::reader::XmlEvent {
+        &self.next
+    }
+
+    fn next(&mut self) -> Result<no_std_xml::reader::XmlEvent, Error> {
+        loop {
+            let e = self.inner.next()?;
+            if let no_std_xml::reader::XmlEvent::Whitespace(_) = &e {
+            } else {
+                break Ok(replace(&mut self.next, e));
+            }
         }
     }
 }
 
 pub fn from_iter<'a, T>(s: impl Iterator<Item=u8> + 'a) -> Result<T, Error> where T: Deserialize<'a>
 {
-    let mut reader = no_std_xml::EventReader::new(s);
+    let mut reader = Reader::new(no_std_xml::EventReader::new(s))?;
     let no_std_xml::reader::XmlEvent::StartDocument { .. } = reader.next()? else {
         return Err(Error::Unexpected { expected: "document start".to_string() });
     };
-    let no_std_xml::reader::XmlEvent::StartElement { name, attributes, .. } = skip_whitespace(&mut reader)? else {
-        return Err(Error::Unexpected { expected: "element start".to_string() });
-    };
-    if name.namespace_ref() != Some(XAML) { return Err(Error::UnknownOrMissingXmlns); }
-    let deserializer = XamlDeserializer {
-        reader: &mut reader,
-        name: name.local_name,
-        attributes: attributes.into_iter(),
-    };
+    let deserializer = XamlDeserializer { reader: &mut reader };
     let res = T::deserialize(deserializer)?;
-    let e = skip_whitespace(&mut reader)?;
+    let e = reader.next()?;
     let no_std_xml::reader::XmlEvent::EndDocument = &e else {
         return Err(Error::Unexpected { expected: format!("document end {e:?}") });
     };
@@ -85,9 +94,9 @@ pub fn from_str<'a, T>(s: &'a str) -> Result<T, Error> where T: Deserialize<'a> 
 }
 
 struct XamlObjectAccess<'a, S: Iterator<Item=u8>> {
-    reader: &'a mut no_std_xml::EventReader<S>,
+    reader: &'a mut Reader<S>,
     name: String,
-    attributes: Option<vec::IntoIter<no_std_xml::attribute::OwnedAttribute>>,
+    attributes: Option<Vec<no_std_xml::attribute::OwnedAttribute>>,
     done: bool,
 }
 
@@ -101,26 +110,37 @@ impl<'a, 'de, S: Iterator<Item=u8> + 'de> de::MapAccess<'de> for XamlObjectAcces
     ) -> Result<Option<K::Value>, Self::Error> where K: DeserializeSeed<'de> {
         if self.done { return Ok(None); }
         self.done = true;
+        let no_std_xml::reader::XmlEvent::StartElement { name, attributes, .. } = self.reader.next()? else {
+            return Err(Error::Unexpected { expected: "element start".to_string() });
+        };
+        if name.namespace_ref() != Some(XAML) { return Err(Error::UnknownOrMissingXmlns); }
+        self.name = name.local_name;
+        self.attributes = Some(attributes);
         let name = seed.deserialize::<StrDeserializer<Self::Error>>(self.name.as_str().into_deserializer())?;
         Ok(Some(name))
     }
     
     fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error> where V: DeserializeSeed<'de> {
         let attributes = self.attributes.take().unwrap();
-        let object_name_prefix = self.name.clone() + ".";
-        seed.deserialize(XamlPropertiesDeserializer {
+        let object_name_prefix = replace(&mut self.name, String::new()) + ".";
+        let res = seed.deserialize(XamlPropertiesDeserializer {
             reader: self.reader,
             object_name_prefix,
             attributes,
-        })
+        })?;
+        let no_std_xml::reader::XmlEvent::EndElement { .. } = self.reader.next()? else {
+            return Err(Error::Unexpected { expected: "element end".to_string() });
+        };
+        Ok(res)
     }
 }
 
 struct XamlPropertiesAccess<'a, S: Iterator<Item=u8>> {
-    reader: &'a mut no_std_xml::EventReader<S>,
+    reader: &'a mut Reader<S>,
     object_name_prefix: String,
     attributes: vec::IntoIter<no_std_xml::attribute::OwnedAttribute>,
-    value: Option<Either<String, (String, Vec<no_std_xml::attribute::OwnedAttribute>)>>,
+    value: Option<String>,
+    full_explicit: bool,
     default_property_name: Option<&'static str>,
     preserve_spaces: bool,
 }
@@ -153,35 +173,39 @@ impl<'a, 'de, S: Iterator<Item=u8> + 'de> de::MapAccess<'de> for XamlPropertiesA
             let property_name = seed.deserialize::<StringDeserializer<Self::Error>>(
                 to_snake(attribute.name.local_name).into_deserializer()
             )?;
-            self.value = Some(Left(attribute.value));
+            self.value = Some(attribute.value);
+            self.full_explicit = false;
             Ok(Some(property_name))
         } else {
-            match skip_whitespace(self.reader) ? {
+            match self.reader.peek() {
                 no_std_xml::reader::XmlEvent::StartElement { name, attributes, .. } => {
                     if name.namespace_ref() != Some(XAML) { return Err(Error::UnknownOrMissingXmlns); }
                     let property_name = if !name.local_name.starts_with(&self.object_name_prefix) {
-                        self.value = Some(Right((name.local_name, attributes)));
                         let Some(default_property_name) = self.default_property_name else {
                             return Err(Error::Unexpected { expected: "property tag".to_string() });
                         };
+                        self.full_explicit = false;
                         default_property_name
                     } else {
                         if !attributes.is_empty() {
                             return Err(Error::Unexpected { expected: "property tag without attributes".to_string() });
                         }
+                        self.full_explicit = true;
                         &name.local_name[self.object_name_prefix.len() ..]
                     };
                     let property_name = to_snake(property_name);
                     let property_name = seed.deserialize::<StringDeserializer<Self::Error>>(
                         property_name.into_deserializer()
                     )?;
+                    if self.full_explicit {
+                        self.reader.next()?;
+                    }
                     Ok(Some(property_name))
                 },
-                no_std_xml::reader::XmlEvent::Characters(mut text) => {
-                    if !self.preserve_spaces {
-                        text = trim_text(&text).to_string();
-                    }
-                    self.value = Some(Left(text));
+                no_std_xml::reader::XmlEvent::Characters(_) => {
+                    let no_std_xml::reader::XmlEvent::Characters(text) = self.reader.next()? else { panic!(); };
+                    self.full_explicit = false;
+                    self.value = Some(if self.preserve_spaces { text } else { trim_text(&text).to_string() });
                     let Some(default_property_name) = self.default_property_name else {
                         return Err(Error::Unexpected { expected: "property tag".to_string() });
                     };
@@ -201,48 +225,23 @@ impl<'a, 'de, S: Iterator<Item=u8> + 'de> de::MapAccess<'de> for XamlPropertiesA
     
     fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error> where V: DeserializeSeed<'de> {
         if let Some(value) = self.value.take() {
-            match value {
-                Left(attribute_value) => seed.deserialize(TextDeserializer { text: attribute_value }),
-                Right((name, attributes)) => {
-                    seed.deserialize(XamlDeserializer {
-                        reader: self.reader,
-                        name,
-                        attributes: attributes.into_iter(),
-                    })
-                },
-            }
+            seed.deserialize(TextDeserializer { text: value })
         } else {
-            match skip_whitespace(self.reader)? {
-                no_std_xml::reader::XmlEvent::StartElement { name, attributes, .. } => {
-                    if name.namespace_ref() != Some(XAML) { return Err(Error::UnknownOrMissingXmlns); }
-                    let res = seed.deserialize(XamlDeserializer {
-                        reader: self.reader,
-                        name: name.local_name,
-                        attributes: attributes.into_iter(),
-                    })?;
-                    let no_std_xml::reader::XmlEvent::EndElement { .. } = skip_whitespace(self.reader)? else {
-                        return Err(Error::Unexpected { expected: "element end".to_string() });
-                    };
-                    Ok(res)
-                },
-                no_std_xml::reader::XmlEvent::Characters(text) => {
-                    let no_std_xml::reader::XmlEvent::EndElement { .. } = skip_whitespace(self.reader)? else {
-                        return Err(Error::Unexpected { expected: "element end".to_string() });
-                    };
-                    seed.deserialize(TextDeserializer { text })
-                },
-                _ => {
-                    Err(Error::Unexpected { expected: "element start or characters".to_string() })
-                },
+            let res = seed.deserialize(XamlDeserializer { reader: self.reader })?;
+            if self.full_explicit {
+                let no_std_xml::reader::XmlEvent::EndElement { .. } = self.reader.next()? else {
+                    return Err(Error::Unexpected { expected: format!("property end") });
+                };
             }
+            Ok(res)
         }
     }
 }
 
 struct XamlPropertiesDeserializer<'a, S: Iterator<Item=u8>> {
-    reader: &'a mut no_std_xml::EventReader<S>,
+    reader: &'a mut Reader<S>,
     object_name_prefix: String,
-    attributes: vec::IntoIter<no_std_xml::attribute::OwnedAttribute>,
+    attributes: Vec<no_std_xml::attribute::OwnedAttribute>,
 }
 
 impl<'a, 'de, S: Iterator<Item=u8> + 'de> Deserializer<'de> for XamlPropertiesDeserializer<'a, S> {
@@ -382,11 +381,12 @@ impl<'a, 'de, S: Iterator<Item=u8> + 'de> Deserializer<'de> for XamlPropertiesDe
         let default_property_name = name.split('@').skip(1).last();
         visitor.visit_map(XamlPropertiesAccess {
             reader: self.reader,
-            attributes: self.attributes,
+            attributes: self.attributes.into_iter(),
             value: None,
             object_name_prefix: self.object_name_prefix,
             default_property_name,
             preserve_spaces: false,
+            full_explicit: false,
         })
     }
 
@@ -398,9 +398,7 @@ impl<'a, 'de, S: Iterator<Item=u8> + 'de> Deserializer<'de> for XamlPropertiesDe
 }
 
 struct XamlDeserializer<'a, S: Iterator<Item=u8>> {
-    reader: &'a mut no_std_xml::EventReader<S>,
-    name: String,
-    attributes: vec::IntoIter<no_std_xml::attribute::OwnedAttribute>,
+    reader: &'a mut Reader<S>,
 }
 
 impl<'a, 'de, S: Iterator<Item=u8> + 'de> Deserializer<'de> for XamlDeserializer<'a, S> {
@@ -521,8 +519,8 @@ impl<'a, 'de, S: Iterator<Item=u8> + 'de> Deserializer<'de> for XamlDeserializer
     fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: de::Visitor<'de> {
         visitor.visit_map(XamlObjectAccess {
             reader: self.reader,
-            name: self.name,
-            attributes: Some(self.attributes),
+            name: String::new(),
+            attributes: None,
             done: false
         })
     }
